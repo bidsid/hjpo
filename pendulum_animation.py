Fixed policy torque/action calculation and made the animation display more information with phase, loss, and time series plots

pendulum_animation.py
@@ -1,3 +1,5 @@
+# Code inspired by the guide at https://matplotlib.org/stable/gallery/animation/double_pendulum.html
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import jax.numpy as jnp
@@ -16,10 +18,18 @@ def swingUpU(t, m, g, l, b, k, umax, theta, thetadot):
     # output = -k * thetadot**2 * delta_E
     output = -k * delta_E * jnp.sign(thetadot + 1e-5)
     output = jnp.clip(output, -umax, umax)
+    # def sigmoid(x,mi, mx): return mi + (mx-mi)*(lambda t: (1+200**(-t+0.5))**(-1) )( (x-mi)/(mx-mi) )
+    # output = sigmoid(output, -umax, umax)
+    # output = umax * jnp.tanh(output)
     
     return output
 
-def simulateWithDiffraxIntegration(ode, t_stop, dt, theta_initial, omega_initial, m, b, L, G, k, umax, policy):
+def generic_torque_calculator_fn(F, t, m, g, l, b, k, umax, theta, thetadot):
+    # F is a forcing function that outputs a torque
+    return F(t, m, g, l, b, k, umax, theta, thetadot)
+
+def simulateWithDiffraxIntegration(ode, torque_calc, t_stop, dt, theta_initial, omega_initial, m, b, L, G, k, 
+                                   umax, policy, loss=None):
     t = jnp.arange(0, t_stop, dt)
 
     term = ODETerm(ode)
@@ -37,37 +47,68 @@ def simulateWithDiffraxIntegration(ode, t_stop, dt, theta_initial, omega_initial
     theta = sol.ys[:,0]
     omega = sol.ys[:,1]
     times = sol.ts
+    energies = 0.5 * m * (L * omega)**2 + m * G * L * (1 - jnp.cos(theta))
+    torques = jnp.array([torque_calc(policy, times[i], m, G, L, b, k, umax, theta[i], omega[i]) for i in range(len(times))])
 
     x = L * jnp.sin(theta)
     y = -L * jnp.cos(theta)
 
-    fig = plt.figure(figsize=(5, 4))
-    ax = fig.add_subplot(autoscale_on=False, xlim=(-L, L), ylim=(-L, 1.))
+    # fig, axs = plt.figure(figsize=(5, 4))
+    fig, axs = plt.subplots(2, 2)
+    fig.suptitle(f"Pendulum data (starting angle {theta_initial} rads, starting angular vel. {omega_initial} rad/s, umax {umax} Nm)")
+    ax = axs[0, 0]
+    ax.set_title("Simulation")
+    ax.set_ylim(-L, 1.)
+    ax.set_xlim(-L, L)
+    # ax = fig.add_subplot(autoscale_on=False, xlim=(-L, L), ylim=(-L, 1.))
     ax.set_aspect('equal')
     ax.grid()
 
     line, = ax.plot([], [], 'o-', lw=2)
     trace, = ax.plot([], [], '.-', lw=1, ms=2)
-    time_template = 'time = %.1fs, energy = %.1fJ'
+    time_template = 'time = %.1fs, energy = %.1fJ, torque = %.1fNm'
     time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
     energy_template = 'Energy = %.1fJ'
-    energy_text = ax.text(.25, .25, '', horizontalalignment='left',
-        verticalalignment='bottom', transform=ax.transAxes)
+    torque_template = '\nTorque = %.1fNm'
+
+    axs[0, 1].set_title("Phase diagram")
+    axs[0, 1].plot(theta, omega)
+    axs[0, 1].set_xlabel("Angle (radians)")
+    axs[0, 1].set_ylabel("Angular velocity (rad/s)")
+    axs[1, 0].set_title("Time series")
+    axs[1, 0].set_xlabel("Time (s)")
+    axs[1, 0].set_ylabel("Value (Radians for angular and Nm for Torque/Energy)")
+    axs[1, 0].plot(times, theta, color="orange", label="Theta")
+    axs[1, 0].plot(times, omega, color="red", label="Omega")
+    axs[1, 0].plot(times, energies, color="green", label="Energy")
+    axs[1, 0].plot(times, torques, color="blue", label="Torque")
+    axs[1, 0].legend()
+    axs[1, 1].set_title("Loss vs iteration")
+    if loss != None:
+        axs[1, 1].plot(loss[0], color="gray", label="Teacher")
+        axs[1, 1].plot(jnp.arange(len(loss[0]), len(loss[0]) + len(loss[1]), 1), loss[1], label="Own loss")
+        axs[1, 1].legend()
+        axs[1, 1].set_xlabel("Epoch number")
+        axs[1, 1].set_ylabel("Loss amount")
     
-    def animate(i, thetas, omegas, x, y, m, g, l):
+    def animate(i, thetas, omegas, times, energies, torques, x, y):
         theta = thetas[i]
         thetadot = omegas[i]
         thisx = [0, x[i]]
         thisy = [0, y[i]]
+        thist = times[i]
+        thisenergy = energies[i]
+        thistorque = torques[i]
 
         line.set_data(thisx, thisy)
-        energy = 0.5 * m * (l * thetadot)**2 + m * g * l * (1 - jnp.cos(theta))
-        time_text.set_text(time_template % (i*dt, energy))
+        # energy = 0.5 * m * (L * thetadot)**2 + m * G * L * (1 - jnp.cos(theta))
+        # torque = torque_calc(policy, thist, m, G, L, b, k, umax, theta, thetadot)
+        time_text.set_text(time_template % (i*dt, thisenergy, thistorque))
 
         return line, trace, time_text
 
     ani = animation.FuncAnimation(
-        fig, animate, len(theta), fargs=(theta, omega, x, y, m, G, L, ), interval=t_stop, blit=True)
+        fig, animate, len(theta), fargs=(theta, omega, times, energies, torques, x, y, ), interval=t_stop, blit=True)
 
     plt.show()
 
@@ -78,7 +119,7 @@ if __name__ == "__main__":
     G = 9.8
     k = 1
     umax = m * G * L / 1.5
-    useSwingUp = False
+    useSwingUp = True
     forcingFunc = swingUpU if useSwingUp else u
     theta_initial = 0
     omega_initial = 1

policy_gradient.py
@@ -82,12 +82,9 @@ def dynamics_eqn_func(theta, omega, t, m, g, l, b, k, umax, F):
     return (F(jnp.array([jnp.sin(theta), jnp.cos(theta), omega])) - b * omega - spring_constant * jnp.sin(theta)) / m
 
 def cost_func(t, theta, omega, currentAction, learnFromAction=None):
-    # currently set to learn from the swing up policy
     goal_arr = jnp.array([theta_goal])
     theta_diff = jnp.mod(theta - goal_arr + jnp.pi, 2 * jnp.pi) - jnp.pi
-    # theta_diff = jnp.array([theta_goal]) - theta
     omega_diff = jnp.array([omega_goal]) - omega
-    action_diff = jnp.array([learnFromAction]) - currentAction
 
     return  theta_diff**2 + 0.01 * omega_diff**2 + 0.001 * currentAction**2
 
@@ -110,7 +107,7 @@ def augmented_ode(t, y, args):
     m, g, l, b, k, umax, F, dynamics_eqn_func, cost_func = args
     dtheta = jnp.array([omega])
     domega = dynamics_eqn_func(theta, omega, t, m, g, l, b, k, umax, F)
-    currentAction = domega
+    currentAction = nn_policy_torque_calc(F, t, m, g, l, b, k, umax, theta, omega)
     swingUpAction = swingUpU(t, m, g, l, b, k, umax, theta, omega)
     dJ = cost_func(t, theta, omega, currentAction, swingUpAction)
 
@@ -155,6 +152,8 @@ def train(policy, optim, epochs, print_every):
     # print("params is ", params)
     opt_state = optim.init(params)
     loss = 0
+    teacher_losses = [0] * int(epochs / 2)
+    selftaught_losses = [0] * int(epochs / 2)
 
     @eqx.filter_jit
     def make_step(policy, cost_function, terminal_cost_function, opt_state, params):
@@ -172,6 +171,7 @@ def train(policy, optim, epochs, print_every):
     ) as t:
         for epoch in range(int(epochs / 2)):
             policy, opt_state, params, loss = make_step(policy, teacher_cost_func, teacher_terminal_cost_func, opt_state, params)
+            teacher_losses[epoch] = loss
             if (epoch % print_every == 0) or (epoch == epochs - 1):
                 print("Most recent loss: ", loss)
 
@@ -180,17 +180,24 @@ def train(policy, optim, epochs, print_every):
     ) as t:
         for epoch in range(int(epochs / 2)):
             policy, opt_state, params, loss = make_step(policy, cost_func, terminal_cost_func, opt_state, params)
+            selftaught_losses[epoch] = loss
             if (epoch % print_every == 0) or (epoch == epochs - 1):
                 print("Most recent loss: ", loss)
     
-    return policy
+    return policy, [teacher_losses, selftaught_losses]
 
+def nn_policy_torque_calc(policy, t, m, g, l, b, k, umax, theta, thetadot):
+    # wrapper function for duck typing
+    # Necessary because generic forces are functions of 9 inputs t through thetadot
+    # but the neural policies in this file take only 1 input which is an array of
+    # sintheta, costheta, omega
+    # so this abstracts getting the torque which allows the pendulum animation file
+    # to have no information on what kind of policy it's getting
+    return jnp.squeeze(policy(jnp.array([jnp.sin(theta), jnp.cos(theta), thetadot])), -1)
 
 if __name__ == "__main__":
     model = Policy(layer_sizes=LAYERS, activation_func=ACTIVATION_FUNC, key=key)
-    print(model)
-    print(jtu.tree_structure(model))
     optim = optax.adam(LEARNING_RATE)
-    finished_model = train(model, optim, EPOCHS, PRINT_EVERY)
-    simulateWithDiffraxIntegration(integration.pendulum_ode_nn, t_stop, dt, theta_initial, omega_initial, m, b, L, G, k, umax, finished_model)
+    finished_model, losses = train(model, optim, EPOCHS, PRINT_EVERY)
+    simulateWithDiffraxIntegration(integration.pendulum_ode_nn, nn_policy_torque_calc, t_stop, dt, theta_initial, omega_initial, m, b, L, G, k, umax, finished_model, losses)
 